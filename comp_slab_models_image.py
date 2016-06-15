Updates to make better plots.  And correct the calculation of the stddev between models (argh).

comp_slab_models_image.py
@@ -207,7 +207,7 @@ def plot_imagegrid(modnames, moddisplaynames, wave, tau, angle,
 
             # quantitative info to save
             cut1_offset = np.average(y)
-            cut1_stddev = np.std(y,ddof=1)
+            cut1_stddev = np.average(abs(y))
             cut1_maxabsdev = np.amax(abs(y))
             
         # second cut (x)
@@ -244,7 +244,7 @@ def plot_imagegrid(modnames, moddisplaynames, wave, tau, angle,
 
             # quantitative info to save
             cut2_offset = np.average(y)
-            cut2_stddev = np.std(y,ddof=1)
+            cut2_stddev = np.average(abs(y))
             cut2_maxabsdev = np.amax(abs(y))
 
         tab.add_row([displaynames[i], i,
@@ -319,7 +319,8 @@ def plot_imagegrid(modnames, moddisplaynames, wave, tau, angle,
     #mpl.cm.register_cmap(name='cubehelix3',
     #                     data=mpl._cm.cubehelix(gamma=1.0, start=0.0, rot=1.0, hue=320.))
     # Heddy's nice cubehelix using the "cubehelix" pacakge (not built into matplotlib
-    custcmap = cubehelix.cmap(reverse = False, rot=1, start=0, startHue=320, sat = 2)
+    custcmap = cubehelix.cmap(reverse = False, rot=1, start=0, 
+                              startHue=320, sat = 2)
 
     # now display the images    
     cutpwidth = 4
@@ -361,7 +362,7 @@ def plot_imagegrid(modnames, moddisplaynames, wave, tau, angle,
         save_name += '_' + save_str
     
     # save the table of the offsets and standard deviations
-    tab.write(save_name+'.dat', format='ascii.commented_header')
+    tab.write('dat/'+save_name+'.dat', format='ascii.commented_header', overwrite=True)
 
     if save_png:
         fig.savefig(save_name+'.png')

comp_slab_models_sed_decompose.py
@@ -15,6 +15,7 @@ import matplotlib.pyplot as pyplot
 import matplotlib.gridspec as gridspec
 
 from astropy.table import Table
+from astropy.stats import sigma_clip
 
 def plot_decompose_sed(modnames, moddisplaynames, tau, angle,
                        single_comp=-1, max_plot_diff=10.0, save_str='',
@@ -100,6 +101,7 @@ def plot_decompose_sed(modnames, moddisplaynames, tau, angle,
             gindxs, = np.where(all_data[j,comp_indxs[k],:] > 0)
             if len(gindxs) >= 2:
                 ave_sed_comps[j,k] = np.median(all_data[j,comp_indxs[k],gindxs])
+                #ave_sed_comps[j,k] = np.average(sigma_clip(all_data[j,comp_indxs[k],gindxs]))
                 ave_sed_comps_npts[j,k] = len(gindxs)
                 #print(all_data[j,k,gindxs])
                 #print(ave_sed_comps[j,k])
@@ -148,7 +150,7 @@ def plot_decompose_sed(modnames, moddisplaynames, tau, angle,
 
                 # compute statistics and output
                 ave_offset = np.average(y)
-                stddev = np.std(y,ddof=1)
+                stddev = np.average(abs(y))
                 maxabsdev = np.amax(abs(y))
                 tab.add_row([label_text[k], k, displaynames[i], i,
                              ave_offset, stddev, maxabsdev])
@@ -206,7 +208,7 @@ def plot_decompose_sed(modnames, moddisplaynames, tau, angle,
         save_name += '_' + save_str
 
     # save the table of the offsets and standard deviations
-    tab.write(save_name+'.dat', format='ascii.commented_header')
+    tab.write('dat/'+save_name+'.dat', format='ascii.commented_header', overwrite=True)
         
     # display the plot
     if save_png:

plot_slab_converge.py
@@ -14,6 +14,46 @@ import matplotlib as mpl
 
 from astropy.table import Table
 
+def plot_converge_slice(ax, taus, waves, angle,
+                        run_tag, kxlabel,
+                        fontsize=16):
+
+    col = ['r','b','g','c']
+    lstyle = ['-','--']
+    for m, tau in enumerate(taus):
+        for n, wave in enumerate(waves):
+            tab_name =  'dat/slab_t' + tau + '_i' + angle + '_w' + wave + \
+                        '_image_comp_' + run_tag
+            
+            cur_table = Table.read(tab_name+'.dat',
+                                   format='ascii.commented_header')
+
+            # extract the number of bins from the model name
+            mvals = np.empty((len(cur_table['model'])))
+            for i, cmodel in enumerate(cur_table['model']):
+                eq_pos = cmodel.find('=')
+                pa_pos = cmodel.find(')')
+                mvals[i] = float(cmodel[eq_pos+1:pa_pos])
+
+            nvals = len(mvals)
+            ax.plot(mvals[1:nvals], cur_table['cut1_stddev'][1:nvals], 
+                    col[n]+lstyle[m],
+                    label=r'$\tau_z = ' + tau + '$; $\lambda = ' + wave + '$')
+            
+    min_val = min(mvals[1:nvals])
+    max_val = max(mvals[1:nvals])
+
+    ax.plot([min_val,max_val],[1.0,1.0],'k--')
+    ax.plot([min_val,max_val],[5.0,5.0],'k-.')
+
+    ax.set_xscale('log')    
+    ax.set_yscale('log')    
+    ax.set_ylim(0.5e-1,1e2)
+    ax.set_ylabel(r'$\sigma$ [%]')
+    ax.set_xlabel(kxlabel)
+    #ax.legend(loc=3)
+
+
 if __name__ == "__main__":
 
     good_angles = ['000','090','180']
@@ -72,41 +112,9 @@ if __name__ == "__main__":
         run_tag = 'dirty_nphot'
         kxlabel = r'$n_p$'
 
-    col = ['r','b','g','c']
-    lstyle = ['-','--']
-    for m, tau in enumerate(taus):
-        for n, wave in enumerate(waves):
-            tab_name =  'slab_t' + tau + '_i' + angle + '_w' + wave + \
-                        '_image_comp_' + run_tag
-            
-            cur_table = Table.read(tab_name+'.dat',
-                                   format='ascii.commented_header')
+    plot_converge_slice(ax, taus, waves, angle, run_tag, kxlabel)
 
-            # extract the number of bins from the model name
-            mvals = np.empty((len(cur_table['model'])))
-            for i, cmodel in enumerate(cur_table['model']):
-                eq_pos = cmodel.find('=')
-                pa_pos = cmodel.find(')')
-                mvals[i] = float(cmodel[eq_pos+1:pa_pos])
-
-            nvals = len(mvals)
-            ax.plot(mvals[1:nvals], cur_table['cut1_stddev'][1:nvals], 
-                    col[n]+lstyle[m],
-                    label=r'$\tau = ' + tau + '; \lambda = ' + wave + '$')
-            
-    min_val = min(mvals[1:nvals])
-    max_val = max(mvals[1:nvals])
-
-    ax.plot([min_val,max_val],[1.0,1.0],'k--')
-    ax.plot([min_val,max_val],[5.0,5.0],'k-.')
-
-    ax.set_xscale('log')    
-    ax.set_yscale('log')    
-    ax.set_ylim(0.5e-1,1e2)
-    ax.set_ylabel(r'$\sigma$ [%]')
-    ax.set_xlabel(kxlabel)
     ax.legend(loc=3)
-
     fig.tight_layout()
 
     save_name = 'slab_converge_' + run_tag + '_i' + angle